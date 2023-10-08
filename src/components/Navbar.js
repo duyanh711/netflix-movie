068419e53c1db5af523c85a8063eaa06b36d58c0
Duy Anh Nguyen <duyanhnguyen71103@gmail.com>
2023-10-08 23:59:52 +0700

    use router

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
         <div className='flex justify-between p-4 absolute w-full items-center z-[100]'>
-            <h1 className='text-red-600 text-4xl font-bold cursor-pointer'>NETFLIX</h1>
+            <Link to="/"><h1 className='text-red-600 text-4xl font-bold cursor-pointer'>NETFLIX</h1></Link>
             <div>
-                <button className='text-white pr-4'>Sign In</button>
-                <button className='text-white px-6 py-2 rounded bg-red-600 cursor-pointer transition hover:bg-red-700'>Sign Up</button>
+                <Link to="/signin"><button className='text-white pr-4'>Sign In</button></Link>
+                <Link to="signup"><button className='text-white px-6 py-2 rounded bg-red-600 cursor-pointer transition hover:bg-red-700'>Sign Up</button></Link>
             </div>
         </div>
     )
