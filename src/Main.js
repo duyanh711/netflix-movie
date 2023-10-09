83eea1c6ebce22b8544444ecb1c42038aa054c78
Duy Anh Nguyen <duyanhnguyen71103@gmail.com>
2023-10-09 23:09:47 +0700

    delete default icon

---
+++ src/Main.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react'
+import requests from './Requests'
+import axios from 'axios'
+
+const Main = () => {
+    const [movies, setMovies] = useState([])
+    // Close API when close app
+    useEffect(() => {
+        axios.get(requests.requestPopular).then((res) => {
+            setMovies(res.data)
+        })
+    }, [])
+
+    console.log(movies)
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Main
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Main from '../Main'
 
 function Home() {
     return (
         <div>
-
+            <Main />
         </div>
     )
 }
