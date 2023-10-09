2c90f6cdf3fcf3bccb7fd37adf11936f58b50bcc
Duy Anh Nguyen <duyanhnguyen71103@gmail.com>
2023-10-09 23:15:05 +0700

    complete API

--- src/Main.js
+++ src/Main.js
@@ -4,12 +4,15 @@ import axios from 'axios'
 
 const Main = () => {
     const [movies, setMovies] = useState([])
+    const movie = movies[Math.floor(Math.random() * movies.length)]
+
     // Close API when close app
     useEffect(() => {
         axios.get(requests.requestPopular).then((res) => {
-            setMovies(res.data)
+            setMovies(res.data.results)
         })
     }, [])
+    console.log(movie)
 
     return (
         <div>
