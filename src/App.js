d3ec1ae24b477ff4d7eed1a4a80ba6a8198aae2f
Duy Anh Nguyen <duyanhnguyen71103@gmail.com>
2023-10-11 00:04:37 +0700

    add protected route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import { AuthContextProvider } from "./context/AuthContext";
 import Account from "./pages/Account";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 
 function App() {
@@ -16,7 +17,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
-          <Route path="/account" element={<Account />} />
+          <Route path="/account" element={<ProtectedRoute><Account /></ProtectedRoute>} />
         </Routes>
       </AuthContextProvider>
     </>
