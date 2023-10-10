31f8eb5c4e5ad83ba0aaf6bd2b5aebb708dfb4b1
Duy Anh Nguyen <duyanhnguyen71103@gmail.com>
2023-10-10 11:35:16 +0700

    complete UI

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,11 +2,11 @@ import React, { useState } from 'react'
 import { FaHeart, FaRegHeart } from 'react-icons/fa'
 
 
-function Movie({ movie, id }) {
+function Movie({ movie }) {
     const [love, setLove] = useState(false)
 
     return (
-        <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:2-[280px] inline-block cursor-pointer relative p-2' key={id}>
+        <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:2-[280px] inline-block cursor-pointer relative p-2'>
             <img className='w-full h-full block' src={`https://image.tmdb.org/t/p/w500/${movie?.backdrop_path}`} alt={movie?.title} />
             <div className='text-white absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100'>
                 <p className='white-space-normal test-xs sm:text-[12px] md:text-[14px] font-bold flex justify-center text-center items-center h-full w-full z-0'>{movie?.title}</p>
--- src/components/Row.js
+++ src/components/Row.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react'
 import Movie from './Movie'
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md'
 
-const Row = ({ title, fetchURL }) => {
+const Row = ({ title, fetchURL, rowId }) => {
 
     const [movies, setMovies] = useState([])
 
@@ -14,18 +14,32 @@ const Row = ({ title, fetchURL }) => {
         )
     }, [fetchURL])
 
+
+    const handleSlideLeft = () => {
+        const sliderElement = document.getElementById('slider' + rowId)
+        sliderElement.scrollLeft -= 500
+    }
+    const handleSlideRight = () => {
+        const sliderElement = document.getElementById('slider' + rowId)
+        sliderElement.scrollLeft += 500
+    }
+
     return (
         <>
             <h2 className='text-white font-bold md:text-xl p-4 md:p-8'>{title}</h2>
             <div className='relative flex items-center group'>
-                <MdChevronLeft size={40} className='  bg-white/30 rounded-full hover:bg-white/100 absolute left-0 overflow-visible group-hover:block hidden cursor-pointer z-10' />
-                <div id={'slider'} className='w-full h-full overflow-x-scroll relative whitespace-nowrap scroll-smooth scrollbar-hide'>
+                <MdChevronLeft onClick={handleSlideLeft}
+                    size={40}
+                    className='  bg-white/30 rounded-full hover:bg-white/100 absolute left-0 overflow-visible group-hover:block hidden cursor-pointer z-10' />
+                <div id={'slider' + rowId} className='w-full h-full overflow-x-scroll relative whitespace-nowrap scroll-smooth scrollbar-hide'>
                     {movies.map((movieItem, id) => (
-                        <Movie movie={movieItem} id={id} />
+                        <Movie movie={movieItem} key={id} />
                     ))
                     }
                 </div>
-                <MdChevronRight size={40} className='  bg-white/30 rounded-full hover:bg-white/100 absolute right-0 overflow-visible group-hover:block hidden cursor-pointer z-10' />
+                <MdChevronRight onClick={handleSlideRight}
+                    size={40}
+                    className='  bg-white/30 rounded-full hover:bg-white/100 absolute right-0 overflow-visible group-hover:block hidden cursor-pointer z-10' />
             </div>
 
         </>
